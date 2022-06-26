dd5ed7254fabb57cbe5ae384e309f3f75974a8a8
yoichi0615 <aaa11655pt@gmail.com>
2022-06-26 18:28:48 +0900

    fist commit

---
+++ src/items/items.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Item } from './item.model';
+import { ItemsService } from './items.service';
+import { ItemStatus } from './item-status.enum';
+
+@Controller('items')
+export class ItemsController {
+  constructor(private readonly ItemsService: ItemsService) {}
+  @Get()
+  findAll() {
+    return this.ItemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.ItemsService.create(item);
+  }
+}
---
+++ src/items/items.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
+
+@Injectable()
+export class ItemsService {
+  private items: Item[] = [];
+
+  findAll() {
+    return 'This is ItemsService';
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
+  }
+}
